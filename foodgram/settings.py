9dc1f80ec50a367d6059ed4eaa14b3829f94bf05
KucJloTa <45943033+KucJloTa@users.noreply.github.com>
2021-02-17 21:01:31 +0300

    71

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ef5x!n1*r+ascb)d08o^k+d^!7os^cqace2u2v_5ta5(s$e_bz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "localhost",
